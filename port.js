fe3c1dff5592baaee44ba5bb51515fb5d5ef3e37
Peio Roth <pierreroth64@gmail.com>
2019-01-09 09:42:18 +0100

    initial commit

---
+++ port.js
@@ -0,0 +1,12 @@
+const SerialPort = require('serialport');
+const port = new SerialPort('/dev/tty.usbserial-14101', {
+  autoOpen: false,
+  baudRate: 115200
+});
+
+port.open(err => {
+  if (err) {
+    return console.log('Error opening port: ', err.message);
+  }
+  console.log('yes! opened!');
+});
